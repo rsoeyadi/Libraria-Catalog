ef920388a673cd53b2312181214ea9650c36ddc9
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-10-18 21:57:43 -0400

    main

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "This microservice is for our library catalog."}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
