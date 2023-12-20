2c1add8fd60077c10a2e7df953b1cdf60f8a36c5
Ryan Soeyadi <rsoeyadi@Ryans-MBP.verizon.net>
2023-12-19 20:30:41 -0500

    changed db

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pymysql
 from flask_cors import CORS
 
 # Set the database credentials
-host = 'database-1.cwruyiuygx34.us-east-2.rds.amazonaws.com'
+host = 'the-best-database.cwruyiuygx34.us-east-2.rds.amazonaws.com'
 port = 3306
 user = 'admin'
-password = 'password'
+password = 'fu3a3A!i#'
 database = 'library_system'
 
 # Connect to the database
